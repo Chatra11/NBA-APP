6c63ef3316859a0ac3af14a4ab96e20f64730a11
Chatra11 <2010817310007@mhs.ulm.ac.id>
2022-06-07 17:47:06 +0800

    perbaikan

--- app/src/main/java/com/uasmobile/player_nba/BindingAdapter.kt
+++ app/src/main/java/com/uasmobile/player_nba/BindingAdapter.kt
@@ -2,12 +2,19 @@ package com.uasmobile.player_nba
 
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.uasmobile.player_nba.network.Player
 import com.uasmobile.player_nba.network.Team
 import com.uasmobile.player_nba.ui.TeamListAdapter
+import com.uasmobile.player_nba.ui.player.PlayerListAdapter
 
 
 @BindingAdapter("ListTeam")
 fun bindRecylerView(recyclerView: RecyclerView,data:List<Team.Data>?){
     val adapter = recyclerView.adapter as TeamListAdapter
     adapter.submitList(data)
+}
+@BindingAdapter("ListPlayer")
+fun bindRecyclerView(recyclerView: RecyclerView,Data:List<Player.Data>){
+    val adapter = recyclerView.adapter as PlayerListAdapter
+    adapter.submitList(Data)
 }
